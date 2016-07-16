2bfef9b932ade11eb2b443faa16264fca0b935ef
memoryleak47 <memoryleak47@protonmail.ch>
2016-07-16 16:31:00 +0200

    ~render-function

--- diagrammer.py
+++ diagrammer.py
@@ -12,6 +12,9 @@ HEADCOLOR = "red"
 BODYCOLOR = "grey"
 EDITCOLOR = "blue"
 
+FONTWIDTH = 12
+FONTHEIGHT = 12
+
 def die(msg):
 	print(msg)
 	sys.exit()
@@ -147,36 +150,73 @@ def getBodySize(node):
 		y = 14 * (1+text.count("\\n"))
 	return x, y
 
+def renderConnection(connection):
+	die("TODO: renderConnection")
+
+def renderEditText(x, y, text, cursor):
+	renderLines(x, y, [line.replace("\\\\", "\\") for line in text.split("\\n")])
+	# TODO render cursor
+
+def renderText(x, y, text):
+	# TODO source-`code`
+	renderLines(x, y, [line.replace("\\\\", "\\") for line in text.split("\\n")])
+
+def renderLines(x, y, lines):
+	global canvas
+
+	for line in lines:
+		canvas.create_text((x, y), anchor="nw", text=line)
+		y += FONTHEIGHT
+
+def renderNodeHead(node, editing=False):
+	global focus, canvas, editdata
+
+	renderPosX = 400 + node["x"] - focus[0]
+	renderPosY = 300 + node["y"] - focus[1]
+	sizeX, sizeY = getHeadSize(node)
+	canvas.create_rectangle(renderPosX - sizeX/2 - PADDING, renderPosY - sizeY/2 - PADDING, renderPosX + sizeX/2 + PADDING, renderPosY + sizeY/2 + PADDING, fill=HEADCOLOR)
+
+	if editing:
+		# render edit environment
+		canvas.create_rectangle(renderPosX - sizeX/2 - PADDING/2, renderPosY - sizeY/2 - PADDING/2, renderPosX + sizeX/2 + PADDING/2, renderPosY + sizeY/2 + PADDING/2, fill=EDITCOLOR)
+		renderEditText(renderPosX - sizeX/2, renderPosY - sizeY/2, editdata['text'], editdata['cursor'])
+	else:
+		renderText(renderPosX - sizeX/2, renderPosY - sizeY/2, node['head'])
+
+def renderNodeBody(node, editing=False):
+	global focus, canvas, editdata
+
+	bodyRenderPosX = 400 + getBodyPosition(node)[0] - focus[0]
+	bodyRenderPosY = 300 + getBodyPosition(node)[1] - focus[1]
+	bodySizeX, bodySizeY = getBodySize(node)
+	canvas.create_rectangle(bodyRenderPosX - bodySizeX/2 - PADDING, bodyRenderPosY - bodySizeY/2 - PADDING, bodyRenderPosX + bodySizeX/2 + PADDING, bodyRenderPosY + bodySizeY/2 + PADDING, fill=BODYCOLOR)
+
+	if editing:
+		canvas.create_rectangle(bodyRenderPosX - bodySizeX/2 - PADDING/2, bodyRenderPosY - bodySizeY/2 - PADDING/2, bodyRenderPosX + bodySizeX/2 + PADDING/2, bodyRenderPosY + bodySizeY/2 + PADDING/2, fill=EDITCOLOR)
+		renderEditText(bodyRenderPosX - bodySizeX/2, bodyRenderPosY - bodySizeY/2, editdata["text"], editdata['cursor'])
+	else:
+		renderText(bodyRenderPosX - bodySizeX/2, bodyRenderPosY - bodySizeY/2, node["body"])
+
+def renderNode(node):
+	global editdata
+	# if node is edited
+	if editdata['object'] == node:
+		renderNodeHead(node, editing=(editdata['type'] == 'node'))
+		if node['status'] == 'open':
+			renderNodeBody(node, editing=(editdata['type'] == 'nodebody'))
+	else:
+		renderNodeHead(node)
+		if node['status'] == 'open':
+			renderNodeBody(node)
+
 def render():
-	global canvas, focus, nodes, connections, editdata
+	global canvas, nodes, connections
 	canvas.delete("all")
 	canvas.create_rectangle(0, 0, 800, 600, fill="white")
 	for connection in connections:
-		canvas.create_line()
+		renderConnection(connection)
 	for node in nodes:
-		renderPosX = 400 + node["x"] - focus[0]
-		renderPosY = 300 + node["y"] - focus[1]
-		sizeX, sizeY = getHeadSize(node)
-		canvas.create_rectangle(renderPosX - sizeX/2 - PADDING, renderPosY - sizeY/2 - PADDING, renderPosX + sizeX/2 + PADDING, renderPosY + sizeY/2 + PADDING, fill=HEADCOLOR)
-
-		# if node is edited
-		if editdata['object'] == node and editdata['type'] == 'node':
-			# render edit environment
-			canvas.create_rectangle(renderPosX - sizeX/2 - PADDING/2, renderPosY - sizeY/2 - PADDING/2, renderPosX + sizeX/2 + PADDING/2, renderPosY + sizeY/2 + PADDING/2, fill=EDITCOLOR)
-			canvas.create_text((renderPosX - sizeX/2, renderPosY - sizeY/2), anchor="nw", text=editdata['text'])
-		else:
-			canvas.create_text((renderPosX - sizeX/2, renderPosY - sizeY/2), anchor="nw", text=node["head"])
-		if node['status'] == 'open':
-			bodyRenderPosX = 400 + getBodyPosition(node)[0] - focus[0]
-			bodyRenderPosY = 300 + getBodyPosition(node)[1] - focus[1]
-			bodySizeX, bodySizeY = getBodySize(node)
-			canvas.create_rectangle(bodyRenderPosX - bodySizeX/2 - PADDING, bodyRenderPosY - bodySizeY/2 - PADDING, bodyRenderPosX + bodySizeX/2 + PADDING, bodyRenderPosY + bodySizeY/2 + PADDING, fill=BODYCOLOR)
-			# if body is edited
-			if editdata['object'] == node and editdata['type'] == 'nodebody':
-				canvas.create_rectangle(bodyRenderPosX - bodySizeX/2 - PADDING/2, bodyRenderPosY - bodySizeY/2 - PADDING/2, bodyRenderPosX + bodySizeX/2 + PADDING/2, bodyRenderPosY + bodySizeY/2 + PADDING/2, fill=EDITCOLOR)
-				canvas.create_text((bodyRenderPosX - bodySizeX/2, bodyRenderPosY - bodySizeY/2), anchor="nw", text=editdata["text"])
-			else:
-				canvas.create_text((bodyRenderPosX - bodySizeX/2, bodyRenderPosY - bodySizeY/2), anchor="nw", text=node["body"])
+		renderNode(node)
 
 def destroyPopup():
 	global popupmenu
@@ -296,6 +336,7 @@ def resetEditdata():
 	editdata['text'] = None
 	editdata['object'] = None
 	editdata['type'] = None
+	editdata['cursor'] = 0
 
 def onKeyPress(event):
 	global editdata
